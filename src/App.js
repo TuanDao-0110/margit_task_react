bb1f2b88b94ecaa867d5b0d71a5ea3bf9fef82d7
tuan <rickytuan1991@gmail.com>
2022-10-26 14:21:27 +0300

    add some thing with reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, incrementByAmount } from './redux/reducer'
+import './App.css'
+// import styles from './Counter.module.css'
+export default function App() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
         >
-          Learn React
-        </a>
-      </header>
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          // onClick={() => dispatch(decrement())}
+          onClick={() => {
+            dispatch(incrementByAmount(500))
+          }}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
