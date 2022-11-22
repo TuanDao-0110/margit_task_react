3d0736e3a4b0192f0fdb398e7f1b01f4f6f39210
tuan <rickytuan1991@gmail.com>
2022-11-22 10:12:28 +0200

    add animals

--- src/animal_project/Animal_page.jsx
+++ src/animal_project/Animal_page.jsx
@@ -3,51 +3,76 @@ import Card from "./Card";
 import { animals, birds } from "./data";
 export default class Animal_page extends Component {
   state = {
-    animals,
-    birds,
+    specie: birds,
+    type: false,
+    load: 5,
   };
   likeFn = (name) => {
-    let index = this.state.birds.findIndex((bird) => bird.name === name);
-    this.setState({ ...this.state.birds[index], likes: this.state.birds[index].likes++ });
+    let index = this.state.specie.findIndex((item) => item.name === name);
+    this.setState({ ...this.state.specie[index], likes: this.state.specie[index].likes++ });
   };
   disLikeFn = (name) => {
-    let index = this.state.birds.findIndex((bird) => bird.name === name);
-    this.setState({ ...this.state.birds[index], likes: this.state.birds[index].likes-- });
+    let index = this.state.specie.findIndex((item) => item.name === name);
+    this.setState({ ...this.state.specie[index], likes: this.state.specie[index].likes-- });
   };
   searching = (value) => {
     if (!value) {
-      this.setState({
-        birds,
-        animals,
+      this.setState(() => {
+        let specie;
+        this.state.type ? (specie = birds) : (specie = animals);
+        return { ...this.state, specie: specie };
       });
     } else {
-      this.setState({ ...this.state, birds: this.state.birds.filter((bird) => bird.name.toLowerCase().includes(value.toLowerCase())) });
+      this.setState({ ...this.state, specie: this.state.specie.filter((item) => item.name.toLowerCase().includes(value.toLowerCase())) });
     }
-    // console.log(this.state.birds.filter((bird) => bird.name.toLowerCase().includes(value.toLowerCase())));
+    // console.log(this.state.specie.filter((bird) => bird.name.toLowerCase().includes(value.toLowerCase())));
   };
   deleteCard = (name) => {
-    this.setState({ ...this.state, birds: this.state.birds.filter((bird) => bird.name !== name) });
+    this.setState({ ...this.state, specie: this.state.specie.filter((item) => item.name !== name) });
   };
   renderedCard = () => {
     return (
       <>
         <div className="flex flex-wrap gap-5 justify-center">
-          {this.state.birds?.map((bird, index) => (
-            <Card key={index} bird={bird} likeFn={this.likeFn} deleteFn={this.deleteCard} disLikeFn={this.disLikeFn}></Card>
+          {this.state.specie?.slice(0, this.state.load).map((item, index) => (
+            <Card key={index} bird={item} likeFn={this.likeFn} deleteFn={this.deleteCard} disLikeFn={this.disLikeFn}></Card>
           ))}
         </div>
       </>
     );
   };
+  loadMore = () => {
+    this.setState({
+      load: this.state.load + 2,
+    });
+  };
   render() {
     return (
       <div>
-        <h1 className="text-center text-lime-800 text-5xl">This is bird project</h1>
-        <h2 className="text-center text-lime-300 text-2xl">Animal : {this.state.birds.length} </h2>
+        <button
+          className="text-4xl bg-teal-400 rounded-md p-5"
+          onClick={() => {
+            this.setState({
+              type: !this.state.type,
+            });
+            this.state.type
+              ? this.setState({
+                  specie: birds,
+                })
+              : this.setState({
+                  specie: animals,
+                });
+          }}
+        >
+          {" "}
+          switch{" "}
+        </button>
+        <h1 className="text-center text-lime-800 text-5xl">This is {!this.state.type ? "bird" : "animals"} project</h1>
+        <h2 className="text-center text-lime-300 text-2xl">Animal : {this.state.specie.length} </h2>
         <div className="flex justify-center">
           <input
-            placeholder="Searching animal by name"
-            className="text-4xl border-2 border-rose-200 w-1/3 p-5 mb-20 mt-10 "
+            placeholder={`Searching ${this.state.type ? "animal" : "bird"} by name`}
+            className="text-4xl border-2 border-rose-200 w-2/3 p-5 mb-20 mt-10 "
             onChange={(e) => {
               this.searching(e.target.value);
             }}
